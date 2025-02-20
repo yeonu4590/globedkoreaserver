c3df0152ec5c49c5d4ba65458fb0cb5128923a19
이연우 <yeonu4590@gmail.com>
2025-02-20 14:30:16 +0900

    커밋 메시지

--- app/layout.js
+++ app/layout.js
@@ -12,7 +12,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata = {
-  title: "Globed Korea Server",
+  title: "Globed Korea Server"
 };
 
 export default function RootLayout({ children }) {
