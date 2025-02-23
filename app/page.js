4f481d60811957607af2fb06c5e9f552c48c5fa8
이연우 <yeonu4590@gmail.com>
2025-02-24 00:19:33 +0900

    커밋 메시지

--- app/page.js
+++ app/page.js
@@ -221,15 +221,19 @@ export default function Home() {
           <p>ping: {ping}</p>
           <br />
           <p>
-            connected user: {users}{" "}
-            <span style={{ color: "gray" }}>
-              {"아니 왜 오류 계속나"}<br></br>
-              {"vercel로 배포했고 next.js입니다."}<br></br>
-              {"그냥 실행하면 되는데 배포본만 오류가 나요 ㅠㅠ"}<br></br>
-              {"왜인지 아시는분은 제 디코로 연락 주세요 (lyw0720)"}
-            </span>
+            connected user: {users}
+            <br></br>
           </p>
         </section>
+        <span style={{ color: "gray" }}>
+          {"아니 왜 오류 계속나"}
+          <br></br>
+          {"vercel로 배포했고 next.js입니다."}
+          <br></br>
+          {"그냥 실행하면 되는데 배포본만 오류가 나요 ㅠㅠ"}
+          <br></br>
+          {"왜인지 아시는분은 제 디코로 연락 주세요 (lyw0720)"}
+        </span>
       </main>
     </div>
   );
